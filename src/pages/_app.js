085f4137d2ca23a8940d646268740b283a40db95
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-08 21:27:06 +0100

    I added the title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
-// pages/_app.js
+import Head from 'next/head';
 import SmoothScrollWrapper from '@/components/SmoothScrollWrapper';
-import Layout from '@/components/Layout';  // Import the Layout component
+import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import "@/styles/navbar.css";
-import "@/styles/scrollbar.css"
+import "@/styles/scrollbar.css";
+
 function MyApp({ Component, pageProps }) {
   return (
     <SmoothScrollWrapper>
       <Layout>
+        <Head>
+          <title>James</title>
+          <meta name="description" content="Default description for my website." />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </SmoothScrollWrapper>
