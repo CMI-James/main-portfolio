899139ddc85482a2a422bd35b31b59b96384d2c6
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-08 22:39:39 +0100

    must fix

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -4,9 +4,8 @@ import React from "react";
 const Services = () => {
   return (
     <Transition>
-      <div className="flex justify-center flex-col items-center h-[300vh] w-full">
+      <div className="flex justify-center flex-col items-center h-screen w-full">
         <p className="text-title">Services</p>
-        <div className="h-screen w-full bg-black"></div>
       </div>
       </Transition>
   );
