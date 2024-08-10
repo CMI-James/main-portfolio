086108c13a523ec03d3919667d51c7ec9030459e
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-11 00:21:16 +0100

    must fix

--- src/pages/memory.jsx
+++ src/pages/memory.jsx
@@ -1,13 +1,13 @@
 import Transition from "@/components/PageTransition/Transition";
 import { useRouter } from "next/router";
 import { useRef, useEffect, useState } from "react";
-import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Memory = () => {
   const router = useRouter();
   const { name } = router.query;
   const audioRef = useRef(null);
-  const [isRedirecting, setIsRedirecting] = useState(false);
+  const [isNavigatingToNothing, setIsNavigatingToNothing] = useState(false);
 
   useEffect(() => {
     if (audioRef.current) {
@@ -18,36 +18,40 @@ const Memory = () => {
   }, []);
 
   useEffect(() => {
-    if (name === undefined && !isRedirecting) {
-      setIsRedirecting(true);
-      // Redirect to /memory-login if name is not present in the query
+    if (name === undefined && !isNavigatingToNothing) {
       router.replace("/memory-login");
     }
-  }, [name, router, isRedirecting]);
+  }, [name, router, isNavigatingToNothing]);
+
+  const [welcomeText] = useTypewriter({
+    words: [` Hey ${name}, Welcome!`],
+    loop: 1,
+    onLoopDone: () => {
+     
+    },
+  });
 
   const [followText] = useTypewriter({
     words: ["Let's head over to memory, shall we?"],
     loop: 1,
     onLoopDone: () => {
       console.log(`loop completed.`);
+      setIsNavigatingToNothing(true);
       setTimeout(() => {
         router.push("/nothing"); // Navigate to /nothing after 2 seconds
       }, 2000);
     },
   });
 
-  if (isRedirecting || name === undefined) {
-    return null; // Optionally, you can show a loading spinner or message while redirecting
-  }
-
   return (
     <Transition>
       <div className="relative px-5 md:px-10 xl:px-20 2xl:px-28 text-center flex flex-col justify-center items-center h-screen w-full">
         <audio ref={audioRef} src="/music/rain.mp3" autoPlay loop />
-        <p className="text-heading lg:text-special">
-          Hey {name}, <br className="lg:hidden" /> Welcome!
+        <p className="text-heading lg:text-special">{welcomeText}</p>
+        <p className="text-2xl">
+          {followText}
+          <Cursor />
         </p>
-        <p className="text-2xl">{followText}<Cursor /></p>
       </div>
     </Transition>
   );
