9ca65b826ef0df2e51a258a6e41d38615abb4516
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-12-20 15:55:01 +0100

    refactor: implemented gallery for my guys

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -62,13 +62,13 @@ const Navbar = ({ controls }) => {
   }, [router.pathname]);
   const navbarVariants = {
     hidden: { y: -100, opacity: 0 },
-    visible: { y: 0, opacity: 1, transition: { duration: 0.8, ease: "easeOut" } },
+    visible: { y: 0, opacity: 1, transition: { duration: 1, ease: "easeOut" } },
   };
 
   const menuVariants = {
     hidden: { opacity: 0, x: "100%" },
-    visible: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeInOut" } },
-    exit: { opacity: 0, x: "100%", transition: { duration: 0.5, ease: "easeInOut" } },
+    visible: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeInOut" } },
+    exit: { opacity: 0, x: "100%", transition: { duration: 1, ease: "easeInOut" } },
   };
   return (
     <motion.div
