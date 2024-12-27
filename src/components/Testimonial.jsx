d405c742fca2e2fa095b9f184aadca7706168fae
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-12-27 23:20:42 +0100

    refactor: implemented gallery for my guys

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import Heading from "./ui/Heading";
 
-
 const Testimonial = () => (
   <section
-  className="h-screen py-[10%] z-10 flex items-start justify-center  "
-  aria-label="testimonial"
->
-  <Heading title="Testimonial" />
-</section>
+    className="h-screen py-[10%] z-10 flex items-start justify-center  "
+    aria-label="testimonial"
+  >
+    <Heading title="Testimonial" />
+  </section>
 );
 
 export default Testimonial;
+  
