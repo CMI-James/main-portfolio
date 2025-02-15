1bf4965cc49728c860f25b7e8caee2327951e948
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-02-15 13:56:40 +0100

    feat: added light/dark mode class to Contact component

--- src/components/Contact/components/ui/reusable.jsx
+++ src/components/Contact/components/ui/reusable.jsx
@@ -64,12 +64,12 @@ const TextAreaField = ({ id, name, rows = 5, label }) => {
         id={id}
         name={name}
         rows={rows}
-        className="peer block w-full appearance-none border-0 border-b border-brown-1000 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
+        className="peer block w-full appearance-none border-0 border-b border-brown-1000 dark:border-beige bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
         placeholder=" "
       ></textarea>
       <label
         htmlFor={id}
-        className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 2xl:text-body-2 text-brown-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75"
+        className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 2xl:text-body-2 text-brown-500 dark:text-beige/50 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75"
       >
         {label}
       </label>
