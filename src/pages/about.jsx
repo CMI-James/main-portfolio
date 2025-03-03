7209d8695f8844e5de7967232b16fcccc56b6c2b
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-03-03 16:06:33 +0100

    refactor: improved homescreen

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,19 +1,11 @@
 import AboutMe from "@/components/About";
-import Section from "@/components/ui/Section";
-import SectionBody from "@/components/ui/SectionBody";
-import SectionHeader from "@/components/ui/SectionHeader";
 import Transition from "@/components/ui/Transition";
 import React from "react";
 
 const About = () => {
   return (
     <Transition className="theme-dark-light">
-      <Section>
-        <SectionHeader title="About" />
-        <SectionBody>
-          <AboutMe />
-        </SectionBody>
-      </Section>
+      <AboutMe />
     </Transition>
   );
 };
