da0108dd66a8c469e90e35edb4b8875ce8ab14c2
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-03-11 23:59:47 +0100

    refactor: updated the section sticky issues

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,6 +15,7 @@ export default function Document() {
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       </Head>
       <body className="theme-dark-light">
+        
         <Main />
         <NextScript />
       </body>
