f2da05bcf47d204f1804d67c9db48facdf9c6817
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-05-22 17:47:02 +0100

    mapped the nav foote

--- src/components/navbar/ui/footer.jsx
+++ src/components/navbar/ui/footer.jsx
@@ -1,36 +1,39 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-
 import { FaXTwitter } from "react-icons/fa6";
+
+const socialLinks = [
+  {
+    href: "https://github.com/CMI-James",
+    icon: <FaGithub />,
+    label: "GitHub",
+  },
+  {
+    href: "https://www.linkedin.com/in/chibuikem-ilonze-7397a522a/",
+    icon: <FaLinkedin />,
+    label: "LinkedIn",
+  },
+  {
+    href: "https://x.com/cmi_james",
+    icon: <FaXTwitter />,
+    label: "Twitter",
+  },
+];
 
 export default function NavFooter() {
   return (
-    <div className="flex w-full justify-around text-2xl">
-      <a
-        href="https://github.com/CMI-James"
-        className="group flex items-center space-x-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FaGithub />
-      </a>
-
-      <a
-        href="https://www.linkedin.com/in/chibuikem-ilonze-7397a522a/"
-        className="group group flex w-fit items-center space-x-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FaLinkedin />
-      </a>
-
-      <a
-        href="https://x.com/cmi_james"
-        className="group flex items-center space-x-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <FaXTwitter />
-      </a>
+    <div className="flex w-full justify-center gap-x-20 text-2xl">
+      {socialLinks.map(({ href, icon, label }) => (
+        <a
+          key={label}
+          href={href}
+          className="group flex items-center space-x-2"
+          target="_blank"
+          rel="noreferrer"
+          aria-label={label}
+        >
+          {icon}
+        </a>
+      ))}
     </div>
   );
 }
