c90734ca166c02e6729384a18f5acd3e8316c2dd
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-05-25 14:53:38 +0100

    removed the zipped file

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -44,7 +44,7 @@ export default function Hero() {
               <motion.p
                 key={index}
                 variants={textVariants}
-                className={`z-10 leading-[6rem] text-8xl sm:leading-[7.5rem] lg:leading-[10rem] font-extrabold  ${
+                className={`z-10 leading-[6rem] text-8xl xl:text-title sm:leading-[7.5rem] lg:leading-[10rem] font-extrabold  ${
                   index === 1
                     ? "transparent-text transition-colors duration-700"
                     : ""
